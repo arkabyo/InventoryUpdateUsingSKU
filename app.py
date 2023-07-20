860e07542a02a4e9c10ec299d4d9ad21209c6548
Avijit Roy Kabyo <10483819+arkabyo@users.noreply.github.com>
2023-07-20 12:15:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,66 @@
-from flask import Flask, jsonify, request
+import pandas as pd
 import requests
 
-app = Flask(__name__)
+# Function to log in and get session token or cookie
+def login(url, password):
+    login_data = {
+        'data[UserExport][password]': password
+    }
+    response = requests.post(url, data=login_data)
+    response.raise_for_status()
+    return response.cookies or response.headers.get('Set-Cookie')
 
-# Vendor login URL and credentials
-login_url = "https://shrugs.com/exports/export_login"
-login_data = {
-    "data[UserExport][password]": "rugsinventory"
-}
+# Function to download the vendor file using session token or cookie
+def download_vendor_file(url, token):
+    headers = {
+        'Cookie': token
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.text
 
-# Vendor inventory URL to download the CSV file
-inventory_url = "https://shrugs.com/exports/inventory_quantity"
+# Process the vendor file data
+def process_vendor_data(vendor_data):
+    vendor_df = pd.read_csv(pd.compat.StringIO(vendor_data))
+    vendor_df['SKU'] = vendor_df['SKU'].str.lower()
+    vendor_df.rename(columns={'Quantity': 'On hand'}, inplace=True)
+    return vendor_df
 
-@app.route('/download_vendor', methods=['POST'])
-def download_vendor_file():
-    # Perform login with credentials
-    session = requests.Session()
-    response = session.post(login_url, data=login_data)
+# Set the vendor URLs and password
+login_url = 'https://shrugs.com/exports/export_login'
+vendor_file_url = 'https://shrugs.com/exports/inventory_quantity'
+VENDOR_PASSWORD = 'YOUR_VENDOR_PASSWORD'
 
-    # Check if login was successful
-    if "Login failed" in response.text:
-        return jsonify({"status": "failed", "message": "Login failed. Please check your credentials."})
+# Get session token or cookie by logging in
+session_token = login(login_url, VENDOR_PASSWORD)
 
-    # Download the CSV file from the vendor inventory URL
-    response = session.get(inventory_url)
+# Download the vendor file using the session token or cookie
+vendor_file_data = download_vendor_file(vendor_file_url, session_token)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Save the CSV content to a file
-        with open("VendorFile.csv", "wb") as file:
-            file.write(response.content)
-        return jsonify({"status": "success", "message": "Vendor file downloaded successfully."})
-    else:
-        return jsonify({"status": "failed", "message": "Failed to download the vendor file."})
+# Load the Shopify file
+shopify_file_path = 'path/to/your/shopify/file.csv'
+shopify_file = pd.read_csv(shopify_file_path)
 
-if __name__ == '__main__':
-    app.run()
+# Convert SKU columns to lowercase for case-insensitive comparison
+shopify_file['SKU'] = shopify_file['SKU'].str.lower()
+
+# Set initial values for "Available" and "On hand" to 0
+shopify_file['Available'] = 0
+shopify_file['On hand'] = 0
+
+# Process the vendor file data
+vendor_df = process_vendor_data(vendor_file_data)
+
+# Compare and update data
+for sku in shopify_file['SKU']:
+    if sku in vendor_df['SKU'].values:
+        quantity = vendor_df.loc[vendor_df['SKU'] == sku, 'On hand'].values[0]
+        shopify_file.loc[shopify_file['SKU'] == sku, 'Available'] = quantity
+        shopify_file.loc[shopify_file['SKU'] == sku, 'On hand'] = quantity
+
+# Save the updated Shopify file to a new CSV file
+updated_shopify_filename = 'UpdatedShopifyFile.csv'
+shopify_file.to_csv(updated_shopify_filename, index=False)
+
+# Print a message indicating the export is complete
+print("Export completed successfully. The updated ShopifyFile is saved as 'UpdatedShopifyFile.csv'.")
