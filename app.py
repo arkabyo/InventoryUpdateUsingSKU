a965e1bdaf0a7a228757923f736279ce302827b4
Avijit Roy Kabyo <10483819+arkabyo@users.noreply.github.com>
2023-07-20 12:18:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import requests
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 # Function to log in and get session token or cookie
 def login(url, password):
@@ -26,41 +29,50 @@ def process_vendor_data(vendor_data):
     vendor_df.rename(columns={'Quantity': 'On hand'}, inplace=True)
     return vendor_df
 
-# Set the vendor URLs and password
-login_url = 'https://shrugs.com/exports/export_login'
-vendor_file_url = 'https://shrugs.com/exports/inventory_quantity'
-VENDOR_PASSWORD = 'YOUR_VENDOR_PASSWORD'
+@app.route('/compare_and_export', methods=['POST'])
+def compare_and_export():
+    # Get Shopify file from HTML form data
+    shopify_file = request.files['shopifyFile']
+    shopify_df = pd.read_csv(shopify_file)
 
-# Get session token or cookie by logging in
-session_token = login(login_url, VENDOR_PASSWORD)
+    # Convert SKU columns to lowercase for case-insensitive comparison
+    shopify_df['SKU'] = shopify_df['SKU'].str.lower()
 
-# Download the vendor file using the session token or cookie
-vendor_file_data = download_vendor_file(vendor_file_url, session_token)
+    # Set initial values for "Available" and "On hand" to 0
+    shopify_df['Available'] = 0
+    shopify_df['On hand'] = 0
 
-# Load the Shopify file
-shopify_file_path = 'path/to/your/shopify/file.csv'
-shopify_file = pd.read_csv(shopify_file_path)
+    # Set the vendor URLs and password
+    login_url = 'https://shrugs.com/exports/export_login'
+    vendor_file_url = 'https://shrugs.com/exports/inventory_quantity'
+    VENDOR_PASSWORD = 'YOUR_VENDOR_PASSWORD'
 
-# Convert SKU columns to lowercase for case-insensitive comparison
-shopify_file['SKU'] = shopify_file['SKU'].str.lower()
+    # Get session token or cookie by logging in
+    session_token = login(login_url, VENDOR_PASSWORD)
 
-# Set initial values for "Available" and "On hand" to 0
-shopify_file['Available'] = 0
-shopify_file['On hand'] = 0
+    # Download the vendor file using the session token or cookie
+    vendor_file_data = download_vendor_file(vendor_file_url, session_token)
 
-# Process the vendor file data
-vendor_df = process_vendor_data(vendor_file_data)
+    # Process the vendor file data
+    vendor_df = process_vendor_data(vendor_file_data)
 
-# Compare and update data
-for sku in shopify_file['SKU']:
-    if sku in vendor_df['SKU'].values:
-        quantity = vendor_df.loc[vendor_df['SKU'] == sku, 'On hand'].values[0]
-        shopify_file.loc[shopify_file['SKU'] == sku, 'Available'] = quantity
-        shopify_file.loc[shopify_file['SKU'] == sku, 'On hand'] = quantity
+    # Compare and update data
+    for sku in shopify_df['SKU']:
+        if sku in vendor_df['SKU'].values:
+            quantity = vendor_df.loc[vendor_df['SKU'] == sku, 'On hand'].values[0]
+            shopify_df.loc[shopify_df['SKU'] == sku, 'Available'] = quantity
+            shopify_df.loc[shopify_df['SKU'] == sku, 'On hand'] = quantity
 
-# Save the updated Shopify file to a new CSV file
-updated_shopify_filename = 'UpdatedShopifyFile.csv'
-shopify_file.to_csv(updated_shopify_filename, index=False)
+    # Save the updated Shopify file to a new CSV file
+    updated_shopify_filename = 'UpdatedShopifyFile.csv'
+    shopify_df.to_csv(updated_shopify_filename, index=False)
 
-# Print a message indicating the export is complete
-print("Export completed successfully. The updated ShopifyFile is saved as 'UpdatedShopifyFile.csv'.")
+    # Provide the link for manual download
+    download_link = f'/content/{updated_shopify_filename}'
+
+    # Return the download link
+    return jsonify({'download_link': download_link})
+
+if __name__ == '__main__':
+    # Run the Flask app
+    app.run(host='0.0.0.0', port=8080)
